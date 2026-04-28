b344b15f252d25e3332b5e8373b43841da7dcc94
aritra <aritra.banerjee.school@gmail.com>
2026-04-28 22:01:32 +0530

    added stamp

--- backend_server/src/main/java/com/backend/backend_server/repository/AuditLogRepository.java
+++ backend_server/src/main/java/com/backend/backend_server/repository/AuditLogRepository.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2026 Aritra Banerjee. All Rights Reserved.
+ * GitHub: https://github.com/programmer1128
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ */
+
 package com.backend.backend_server.repository;
 
 import com.backend.backend_server.entity.AuditLog;
